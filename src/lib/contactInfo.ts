51c4c510907dc5ade9640bc896143aa5cc428a17
ZÜMA <zuma.urbanwear@gmail.com>
2026-07-12 14:38:59 +0100

    Add files via upload

---
+++ src/lib/contactInfo.ts
@@ -0,0 +1,5 @@
+// Single source of truth for ZUMA's public contact details.
+export const CONTACT_EMAIL = "zuma.urbanwear@gmail.com";
+export const INSTAGRAM_HANDLE = "@zumaurbanwear";
+export const INSTAGRAM_URL = "https://www.instagram.com/zumaurbanwear";
+export const TIKTOK_URL = "https://www.tiktok.com/@zumaurbanwear";
